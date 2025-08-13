01092f2a0eeb3d341c7302ede56865f70d730d55
syukranDev <m.syukransoleh@amast.com.my>
2025-08-13 14:03:12 +0800

    set backend base url for UI

--- client_admin/src/services/api.js
+++ client_admin/src/services/api.js
@@ -1,10 +1,8 @@
 import axios from 'axios'
 
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || import.meta.env.VITE_API_BASE_URL_LOCAL || 'http://localhost:3113'
+const API_BASE_URL = `https://asessment-adv.onrender.com`
+// const API_BASE_URL = `http://localhost:3113`
 
-
-//prod debug
-console.log(import.meta.env.VITE_API_BASE_URL, import.meta.env.VITE_API_BASE_URL_LOCAL, VITE_API_BASE_URL)
 const api = axios.create({
   baseURL: API_BASE_URL,
   headers: {
