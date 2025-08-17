187da85569d27998eccc9e873115c94020b584c6
syukranDev <m.syukransoleh@amast.com.my>
2025-08-18 01:02:41 +0800

    add loading note upon first time login prod

--- client_admin/src/views/LoginView.vue
+++ client_admin/src/views/LoginView.vue
@@ -8,6 +8,14 @@
         <p class="mt-2 text-center text-sm text-gray-600">
           Sign in to access the admin dashboard
         </p>
+        <!-- notedev: this is for PROD usage-->
+        <div v-if="loading" class="flex justify-center my-4">
+          <svg class="animate-spin h-5 w-5 text-indigo-600" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+            <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+            <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8v8z"></path>
+          </svg>
+          <span class="ml-2 text-indigo-600">Please wait, waking up free-tier server (appx. 60 seconds)...</span> 
+        </div>
       </div>
       
       <form class="mt-8 space-y-6" @submit.prevent="handleLogin">
